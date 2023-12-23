ad8aad50b18a0f4e52f08718303d41095a09b807
FemiJopseho <josepholuwafemi2014@gmail.com>
2023-12-23 08:22:52 +0100

    INVESTMENT-HEADER completed
    did not include changes to other aspect of website in this commit

---
+++ pages/js-components/footer__currentyear__auto.js
@@ -0,0 +1,4 @@
+// this code simply auto changes the year in the footer to the current year.
+
+var currentYear = new Date().getFullYear();
+document.getElementById("current__year").textContent = currentYear;
