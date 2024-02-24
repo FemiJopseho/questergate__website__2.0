715d5e35cc50ae54097460b72ad3bb3fccea0a12
FemiJopseho <josepholuwafemi2014@gmail.com>
2024-02-24 15:37:48 +0100

    fixed testimonial and general layouts

---
+++ pages/js-components/carousel--swipper--jbg-1..js
@@ -0,0 +1,63 @@
+/**
+  Swiper:
+  https://swiperjs.com/
+**/
+const swiper = new Swiper(".swiperCarousel", {
+	slidesPerView: 3,
+	centeredSlides: true,
+	spaceBetween: 15, //space btw each -swiper-slide
+	keyboard: {
+		enabled: true,
+	},
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+const slides = document.getElementsByClassName("swiper-slide");
+for (const slide of slides) {
+	slide.addEventListener("click", () => {
+		const { className } = slide;
+		if (className.includes("swiper-slide-next")) {
+			swiper.slideNext();
+		} else if (className.includes("swiper-slide-prev")) {
+			swiper.slidePrev();
+		}
+	});
+}
+
+// /////////////////////////////////
+// Add a button with class "prev" and title "previous"
+const prevButton = document.querySelector(".prev");
+if (prevButton) {
+	prevButton.addEventListener("click", () => {
+		swiper.slidePrev();
+	});
+}
+
+// Add a button with class "next" and title "next"
+const nextButton = document.querySelector(".next");
+if (nextButton) {
+	nextButton.addEventListener("click", () => {
+		swiper.slideNext();
+	});
+}
+// ///////////////////////////////////
+
+function resizeTextToFit() {
+	const quoteEls = document.getElementsByClassName("quote");
+	for (const el of quoteEls) {
+		el.style.width = el.offsetWidth;
+		el.style.height = el.offsetHeight;
+	}
+	textFit(quoteEls, { maxFontSize: 14 });
+}
+resizeTextToFit();
+addEventListener("resize", (event) => {
+	resizeTextToFit();
+});
